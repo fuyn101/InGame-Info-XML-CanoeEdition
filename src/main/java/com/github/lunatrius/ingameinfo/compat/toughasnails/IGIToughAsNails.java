a45fd11fdb640ee8ada912aeaaa6b3924ace3a00
Lonelyxiya <3438505487@qq.com>
2026-02-07 01:52:29 +0800

    remove

--- src/main/java/com/github/lunatrius/ingameinfo/compat/toughasnails/IGIToughAsNails.java
+++
@@ -1,8 +0,0 @@
-package com.github.lunatrius.ingameinfo.compat.toughasnails;
-
-import net.minecraftforge.fml.common.ModMetadata;
-
-public class IGIToughAsNails
-{
-    public static ModMetadata metadata;
-}
