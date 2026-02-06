20fbf00c5a3d6f79bb28e22f2af5999a2b0cc417
Lonelyxiya <3438505487@qq.com>
2026-02-06 16:55:45 +0800

    Remove VersionCheck

--- src/main/java/com/github/lunatrius/core/handler/ConfigurationHandler.java
+++ src/main/java/com/github/lunatrius/core/handler/ConfigurationHandler.java
@@ -1,10 +1,7 @@
 package com.github.lunatrius.core.handler;
 
-import com.github.lunatrius.core.reference.Names;
 import com.github.lunatrius.core.reference.Reference;
 import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.Config.Comment;
-import net.minecraftforge.common.config.Config.RequiresMcRestart;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
--- src/main/java/com/github/lunatrius/core/reference/Reference.java
+++ src/main/java/com/github/lunatrius/core/reference/Reference.java
@@ -7,8 +7,6 @@ public class Reference {
     public static final String MODID = "lunatriuscore";
     public static final String NAME = "LunatriusCore";
     public static final String VERSION = "${version}";
-    public static final String FORGE = "${forgeversion}";
-    public static final String MINECRAFT = "${mcversion}";
     public static final String PROXY_SERVER = "com.github.lunatrius.core.proxy.ServerProxy";
     public static final String PROXY_CLIENT = "com.github.lunatrius.core.proxy.ClientProxy";
 
